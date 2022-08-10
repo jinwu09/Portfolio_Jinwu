8f3758bd8f697ccb7ba112ba35f6ab97e11521ec
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-08-10 21:02:22 +0800

    update progress

---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="text-skin-base">
+    <div class="flex">
+      <p>Porftolio</p>
+    </div>
+    <div class="flex bg-skin-fill-accent">
+      <h1 class="m-auto">Welcome</h1>
+    </div>
+    <div class="card grid grid-flow-col grid-cols-3">
+      <div class="border-2">
+        <p>image here</p>
+      </div>
+      <div class="col-span-3 pl-6">
+        <p>Albert John B. Santos</p>
+        <p>Known Porgraming Language</p>
+        <p>Dart</p>
+        <p>C</p>
+        <p>Known Web Framework stack</p>
+        <p>VueJS, Vue Router, Pinia,</p>
+        <p>Flutter</p>
+      </div>
+    </div>
+  </div>
+</template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,30 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0b132b",
+        secondary: "#fffded",
+        third: "#4d7298",
+      },
+      textColor: {
+        skin: {
+          base: "var(--color-text-base)",
+          muted: "var(--color-text-muted)",
+          inverted: "var(--color-text-inverted)",
+          navigation: "var(--color-text-navigation)",
+        },
+      },
+      backgroundColor: {
+        skin: {
+          fill: "var(--color-fill)",
+          "fill-accent": "var(--color-fill-accent)",
+          focus: "var(--color-fill-focus)",
+          "button-accent": "var(--color-button-accent)",
+          "button-accent-hover": "var(--color-button-accent-hover)",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
