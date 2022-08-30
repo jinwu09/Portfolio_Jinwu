fa8ff493c2addcd2825da75240ab461a631d5f1d
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-08-30 16:40:32 +0800

    Revert "final test for github pages"
    This reverts commit 05b8a9b84f393fb1c6e0a5c4900babbba4cf17f3.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio_Jinwu/",
+  base: "/Portfolio/",
   plugins: [vue()],
   resolve: {
     alias: {
