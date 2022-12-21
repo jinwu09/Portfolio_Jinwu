9a652f2318ca07b3cb25f19f395660938361597e
jinwu09 <61149832+jinwu09@users.noreply.github.com>
2022-12-21 15:54:26 +0800

    update

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -6,11 +6,12 @@ const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: "/Home",
+      path: "/",
       name: "Home",
+      alias: ["/index.html"],
       component: HomeVue,
     },
-    { path: "/", redirect: "Home" },
+    // { path: "/", redirect: "Home" },
     {
       path: "/Privacy_Policy",
       name: "Privacy_Policy",
--- vue/vite.config.js
+++ vue/vite.config.js
@@ -5,7 +5,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Portfolio_Jinwu/",
+  // base: "/index.html",
+  // base: "/Portfolio_Jinwu/",
   plugins: [vue()],
   resolve: {
     alias: {
